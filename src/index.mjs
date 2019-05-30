beb2fd6cad8d77210fd4c8d9b1e0b7fe1ad628a1
Jascha <jascha@jaeh.at>
2019-05-30 19:32:19 +0200

    do not error on requestanimationframe

--- src/index.mjs
+++ src/index.mjs
@@ -6,7 +6,7 @@ var EMPTY_ARR = []
 
 var map = EMPTY_ARR.map
 var isArray = Array.isArray
-var defer = requestAnimationFrame || setTimeout
+var defer = typeof requestAnimationFrame !== 'undefined' ? requestAnimationFrame : setTimeout
 
 var createClass = function(obj) {
   var out = ""
